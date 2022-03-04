967892025ed41db89cd79008dec8852f4bd4c9a7
Joel <joelphilip13@gmail.com>
2022-03-04 22:57:43 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
 
 import FeedbackData from "./data/FeedbackData";
 
@@ -24,14 +27,30 @@ function App() {
   };
 
   return (
-    <div>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedbacks={feedbacks} />
-        <FeedbackList feedbacks={feedbacks} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <div>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedbacks={feedbacks} />
+                <FeedbackList
+                  feedbacks={feedbacks}
+                  handleDelete={deleteFeedback}
+                />
+              </div>
+            }
+          />
+
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
       </div>
-    </div>
+      <AboutIconLink />
+    </Router>
   );
 }
 
